bd7fed6a851c86b3465e9b4f58c46b99ba6c2d95
suneettipirneni <suneettipirneni@icloud.com>
2023-04-23 01:32:44 -0400

    skip 0 masks

--- train.py
+++ train.py
@@ -56,6 +56,11 @@ def train(trainloader: DataLoader, testloader: DataLoader, device: torch.device,
         labels: torch.Tensor = item['label'].to(device)
         images: torch.Tensor = item['image'].to(device)
 
+        # HD metrics can't use zero masks but they are in the dataset
+        # so skip them.
+        if 0 == labels.count_nonzero():
+          continue
+
         outputs: torch.Tensor = inference(model, images)
         outputs = torch.stack([post_trans(i) for i in decollate_batch(outputs)])
         dice_metric(outputs, labels)
