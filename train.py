257f504ae2664d31297f36271de2afab0f13074a
suneettipirneni <suneettipirneni@icloud.com>
2023-04-23 00:06:49 -0400

    logging

--- train.py
+++ train.py
@@ -61,6 +61,8 @@ def train(trainloader: DataLoader, testloader: DataLoader, device: torch.device,
         dice_metric(outputs, labels)
         print(f"outputs type = {type(outputs)}")
         print(f"labels type = {type(labels)}") 
+        print(np.array(outputs))
+        print(labels.detach().cpu().numpy())
         epoch_distances.append(hd(np.array(outputs), labels.detach().cpu().numpy()))
 
       # Print accuracy
