5c6598c41f99b4802df83acbc7f62ae3280cca53
suneettipirneni <suneettipirneni@icloud.com>
2023-04-23 00:36:30 -0400

    use dtype

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ def train(trainloader: DataLoader, testloader: DataLoader, device: torch.device,
         images: torch.Tensor = item['image'].to(device)
 
         outputs: torch.Tensor = inference(model, images) 
-        outputs = torch.Tensor(np.fromiter([post_trans(i) for i in decollate_batch(outputs)]))
+        outputs = torch.Tensor(np.fromiter([post_trans(i) for i in decollate_batch(outputs)], int))
         dice_metric(outputs, labels)
         print(f"outputs type = {type(outputs)}")
         print(f"labels type = {type(labels)}") 
