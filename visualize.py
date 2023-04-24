76cf013cb73b4eb9f282f8fd425a5a6676cd5748
suneettipirneni <suneettipirneni@icloud.com>
2023-04-24 15:43:58 -0400

    fix sizes

--- visualize.py
+++ visualize.py
@@ -25,7 +25,7 @@ def visualize(model: torch.nn.Module, device):
   num_input_channels = 4
 
   plt.figure("input")
-  for channel in range(num_input_channels):
+  for channel in range(num_input_channels, fig_size):
     plt.subplot(1, num_input_channels, channel + 1)
     plt.title(f"Input channel {channel}")
     plt.imshow(validation_dataset[index]['image'][channel, :, :, frame].detach().cpu(), cmap="gray")
