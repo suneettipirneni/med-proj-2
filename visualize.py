dfe507eacaee1e7e827172868b2fb97c28c06567
suneettipirneni <suneettipirneni@icloud.com>
2023-04-24 14:38:27 -0400

    fix sizes

--- visualize.py
+++ visualize.py
@@ -12,16 +12,20 @@ def visualize(dataset: Dataset, model: torch.nn.Module, device):
   fig_size = (24, 6)
   num_pred_channels = 3
 
-  plt.figure("Predicted Mask", fig_size)
+  plt.figure("Predicted Mask", (18, 16))
   for channel in range(num_pred_channels):
     plt.subplot(1, num_pred_channels, channel + 1)
+    plt.title(f"Predicted mask channel {channel}")
     plt.imshow(predicted[channel, :, :, frame].detach().cpu(), cmap="gray")
-    plt.savefig("vis.png")
 
-  plt.figure("Ground Truth Mask", fig_size)
+  plt.savefig("pred.png")
+
+  plt.figure("Ground Truth Mask", (18, 16))
   for channel in range(num_pred_channels):
     plt.subplot(1, num_pred_channels, channel + 1)
+    plt.title(f"GT mask channel {channel}")
     plt.imshow(dataset[index]['label'][channel, :, :, frame].detach().cpu(), cmap="gray")
-    plt.savefig("vis.png") 
+    
+  plt.savefig("gt.png") 
 
 
