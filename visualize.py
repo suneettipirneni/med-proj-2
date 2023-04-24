51e93086f73939b4b79a9dd0403839db5ff0b4de
suneettipirneni <suneettipirneni@icloud.com>
2023-04-24 14:23:04 -0400

    .

--- visualize.py
+++ visualize.py
@@ -10,11 +10,7 @@ def visualize(dataset: Dataset, model: torch.nn.Module, device):
   input = dataset[0]['image'].unsqueeze(0).to(device)
   predicted = post_trans(inference(model, input)[0])
 
-  plt.figure("Evaluation")
-
-  plt.subplot(1, 2)
   plt.imshow(predicted[channel, :, :, frame])
-
   plt.savefig("vis.png")
 
 
