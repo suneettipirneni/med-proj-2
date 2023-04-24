0914d7b34c50350c172aaedabf799d5f0d0760cf
suneettipirneni <suneettipirneni@icloud.com>
2023-04-24 14:19:42 -0400

    fix visualize

--- visualize.py
+++ visualize.py
@@ -2,13 +2,13 @@ from torch.utils.data import Dataset
 import torch
 import matplotlib.pyplot as plt
 
-from util import inference
+from util import inference, post_trans
 
-def visualize(dataset: Dataset, model: torch.nn.Module):
+def visualize(dataset: Dataset, model: torch.nn.Module, device):
   frame = 60
   channel = 0
-  input = dataset[0]['image']
-  predicted = inference(model, input)
+  input = dataset[0]['image'].unsqueeze(0).to(device)
+  predicted = post_trans(inference(model, input)[0])
 
   plt.figure("Evaluation")
 
